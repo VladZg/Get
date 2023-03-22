7ea20b0a4b5f4f987d15805147d43add8a1ad52e
Zagorodnyuk Vladislav <vlad.v.zag@gmail.com>
2023-03-22 16:56:41 +0300

    first task done

--- 4-ЦАП/4-1-dac.py
+++ 4-ЦАП/4-1-dac.py
@@ -1,29 +1,32 @@
-#  import GPiO as
+import RPi.GPIO as GPIO
+import funcs
 
-dac = []    # list of GPIO-pins from DAC
+GPIO.setwarnings(False)
 
-#
-# GPIO.setmode
-#
-#
+dac = [26, 19, 13, 6, 5, 11, 9, 10]    # list of GPIO-pins from DAC
 
-def Dec2Bin(num):
-    return [int(elem) for elem in bin(num)[2:].zfill(8)]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
 
-while True:
-    try:
-        while True:
-            num = input("Type a number from 0 to 255: ")
-            if 0 <= int(num) <= 255:
-                print(Dec2Bin(int(num)))
+try:
+    while True:
+        num = input("Type a number from 0 to 255: ")
+        try:
+            num = int(num)
+            if 0 <= num <= 255:
+                GPIO.output(dac, funcs.Dec2Bin(num))
+                voltage = float(num) / 256.0 * 3.3
+                print(f"Output voltage is about {voltage:.4} volt")
             else:
-                print("Number is out of range [0,255]!")
-    except:
-        if num == "q":
-            break
-        print("Not an integer!")
+                if num < 0:
+                    print("Number have to be >=0! Try again...")
+                elif num > 255:
+                    print("Number is out of range [0,255]! Try again...")  
+        except:
+            if num == "q": break
+            print("You have to type a number, not string! Try again...")
 
-# GPIO.cleanup()
-#
-#
-print("EOP")
+finally:
+    GPIO.output(dac, 0)
+    GPIO.cleanup()
+    print("EOP")
---
+++ 4-ЦАП/funcs.py
@@ -0,0 +1,2 @@
+def Dec2Bin(num):
+    return [int(elem) for elem in bin(num)[2:].zfill(8)]
